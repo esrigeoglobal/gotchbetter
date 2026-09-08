692be9a6371585e9430665caba8d7fc50649a3f9
esrigeoglobal <ejordonez.digm@gmail.com>
2026-09-08 12:04:28 -0600

    featrue(profile):#12 connection macos --> 8 2-2026

--- scripts/macos_connection.sh
+++ scripts/macos_connection.sh
@@ -16,6 +16,38 @@ fi
 
 echo "sharing connecting from upstream interface $UPSTREAM_IFACE to usb interface $USB_IFACE ..."
 
+
+
+set -e
+
+#Default 
+port=22
+USER=""
+HOST=""
+IDENTITY=""
+COMMAND=""
+
+
+#Help function
+usage() {
+
+Usage: $0 -u USER -h HOST [OPTIONS]
+
+Required:
+  -u USER      Remote username
+  -h HOST      Remote host (IP or domain)
+
+Options:
+  -p PORT      SSH port (default: 22)
+  -i FILE      Path to private key file
+  -c "CMD"     Execute a command after login (instead of interactive shell)
+  -?           Show this help
+
+Example:
+  $0 -u john -h 192.168.1.100 -i ~/.ssh/id_rsa
+  $0 -u admin -h example.com -p 2222 -c "ls -la /var/log"
+
+
 sysctl -w net.inet.ip.forwarding=1
 pfctl -e
 echo "nat on ${UPSTREAM_IFACE} from ${USB_IFACE}:network to any -> (${UPSTREAM_IFACE})" | pfctl -f -
